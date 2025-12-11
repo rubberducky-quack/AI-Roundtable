0ee35e46fae4a0d1f3fb96374d5ff72d1c376f9c
Gianni Caputo <giannicaputo5@gmail.com>
2025-12-11 15:52:15 +0100

    chore: update model definitions in `main.py` to use uniform configuration

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import ollama
 
 # Modelle definieren
 #model_a = "ministral-3:14b"
-#model_b = "deepseek-r1:8b"
+#model_b = "ministral-3:14b"
+#model_c ="ministral-3:14b"
 model_a = "ministral-3:3b"
 model_b = "ministral-3:3b"
 model_c = "ministral-3:3b"
